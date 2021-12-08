a8a8135f0df17e5f0a25cf01c60cdab9e37fbf50
dzmitriy78 <dzmitriy1978@gmail.com>
2021-12-08 23:18:10 +0300

    Buttons

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,38 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import {Buttons} from "./Buttons";
 
 function App() {
+
+    let [count, setCount] = useState(0)
+
+    function onCount() {
+        if (count < 5) {
+            count += 1
+        } else {
+            count+=0;
+        }
+        setCount(count)
+    }
+    function onReset() {
+        count = 0
+        setCount(count)
+    }
+
     return (
 
         <div className="App">
             <div>
-                <Buttons/>
-                <input type={"text"}/>
+                <div className={count===5? "large" : ""}>
+                    {count}
+                </div>
+
+                <Buttons onCount = {onCount}
+                         onReset = {onReset}
+                         count = {count}
+
+                />
             </div>
-
-
 
         </div>
     );
--- src/Buttons.tsx
+++ src/Buttons.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 
-export function Buttons() {
-    return (
+export function Buttons(props: any) {
+        return (
+
         <div>
-            <button>int</button>
-            <button>reset</button>
+            <button disabled={props.count === 5} onClick={()=>props.onCount()}>int</button>
+            <button disabled={props.count===0} onClick={()=>props.onReset()}>reset</button>
         </div>
     )
 }
