e174fa25690072bb08d2191586ba47c6227890e1
dzmitriy78 <dzmitriy1978@gmail.com>
2021-12-08 21:07:37 +0300

    Button

---
+++ src/Buttons.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export function Buttons() {
+    return (
+        <div>
+            <button>int</button>
+            <button>reset</button>
+        </div>
+    )
+}
